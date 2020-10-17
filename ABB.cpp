ad46448d80d912148f71d3e416cd6ec363c414d3
Amanda Albuquerque <albuquerque@localhost.localdomain>
2020-10-17 20:21:30 -0300

    atributos

--- ABB.cpp
+++ ABB.cpp
@@ -13,11 +13,18 @@ class ABB_ {
 		public:
 
 			struct node_{
+				int chave;
+				node_ *esquerdo = nullptr; //ponteiro para o filho esquerdo
+				node_ *direito = nullptr;
+				node_ *pai; 
+				size_t altura;
+
+				node_(int chave = 0, node_* p = nullptr, node_* esq = nullptr, node_* dir = nullptr): chave(chave), pai(p), esquerdo(esq), direito(dir), altura(0){ }
 
 			};
 
 		private:
 
-			
+
 
 }
