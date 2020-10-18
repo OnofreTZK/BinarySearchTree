934e1af296d830f14a7cc89beaa7819897f56f07
OnofreTZK <tiagojack84@gmail.com>
2020-10-18 18:22:29 -0300

    search and insert

---
+++ build/CMakeFiles/abb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abb.dir/src/bst.cpp.o"
+  "CMakeFiles/abb.dir/src/main.cpp.o"
+  "abb"
+  "abb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
